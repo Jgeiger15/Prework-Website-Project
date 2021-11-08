4d7a080c7b49b7decd6ce5de0abd621dd2dd9235
Jakob Geiger <jakobgeiger15@gmail.com>
2021-11-07 22:42:11 -0500

    completed website

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome to Jakob's Website!";
 document.querySelector("body").appendChild(h2);
+
+const image = document.getElementById("Jakob");
+
+document.addEventListener("click", function(){ alert("Hey! That hurt!"); });
